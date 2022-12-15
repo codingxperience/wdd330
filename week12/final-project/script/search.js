c5396abdfed70cb62aad0d241a2c475683f94f28
codingxperience <oko22002@byui.edu>
2022-12-15 07:41:05 +0300

    final project complete

---
+++ week12/final-project/script/search.js
@@ -0,0 +1,26 @@
+// date function
+let currentFullDate = document.lastModified;
+let year = new Date().getFullYear();
+let unavailable = document.getElementById("notAvailable");
+document.getElementById("year").textContent = year;
+unavailable.style.display = "none";
+
+// Search function
+const searchBar = document.forms["search-books"].querySelector("input");
+searchBar.addEventListener("keyup", function (e) {
+  const term = e.target.value.toLocaleLowerCase();
+  const searchTag = document.getElementsByTagName("section");
+  let notAvailable = document.getElementById("notAvailable");
+
+  let hasResults = false;
+  Array.from(searchTag).forEach(function (searchTag) {
+    const title = searchTag.textContent;
+    if (title.toLowerCase().indexOf(term) != -1) {
+      searchTag.style.display = "";
+      hasResults = true;
+    } else {
+      searchTag.style.display = "none";
+    }
+  });
+  notAvailable.style.display = hasResults ? "none" : "block";
+});
